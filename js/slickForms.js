dd18c8b36e573626919a55c60b56f0b6a3670f7c
Samuel - Work <s-bridge@live.co.uk>
2014-06-22 11:55:54 +0100

    [JS] - Initial set up

---
+++ js/slickForms.js
@@ -0,0 +1,31 @@
+(function() {
+	
+	'use strict';
+
+	var slickForms = function() {
+
+		var self = this;
+
+		self.public_methods = {
+
+
+
+		}
+
+		var private_methods = {
+
+			initialise: function() {
+
+				
+
+			}
+
+		}
+
+		private_methods.initialise();
+
+	}
+	
+	var slick = new slickForms();
+
+})();
